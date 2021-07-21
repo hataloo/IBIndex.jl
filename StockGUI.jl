3e3b4909926fae168a995da924a590e6e093e954
hataloo <jacksandberg998@gmail.com>
2021-07-21 19:40:18 +0200

    Pre-MVC refactor

--- StockGUI.jl
+++ StockGUI.jl
@@ -1,6 +1,11 @@
 using GLMakie
-
+include("StockParse.jl")
 #gui = Figure(resolution = (1800, 900))
+
+toBuy, moneyLeft = calculateNumberOfStocksToBuy(22500.0, IBIndex, currentPortfolio)
+
+newPortfolio = OrderedDict{String, Int64}([k => v + toBuy[k] for (k,v) in currentPortfolio])
+
 
 scene, layout = layoutscene(resolution = (1600,1200))
 
@@ -11,25 +16,25 @@ scrollButtonLayout = GridLayout()
 
 layout[1:1,2] = buttonLayout
 
+highlightedStockTicker = Node(IBIndex[1][1].ticker)
 
-
-numberOfVisibleOptions = 5
+numberOfVisibleOptions = 8
 for i = 1:numberOfVisibleOptions
     stock, weight = IBIndex[i]
-    buttonLayout[i+1,2] = Button(scene, label = "$(stock.ticker)")
+    buttonLayout[i+1,2] = stockButton =  Button(scene, label = "$(stock.ticker)")
+    on(stockButton.clicks) do x
+        highlightedStockTicker[] = stockButton.label[]
+    end
     buttonLayout[i+1,3] = Label(scene, "$(currentPortfolio[stock.ticker])")
     buttonLayout[i+1,4] = Label(scene, "$(toBuy[stock.ticker])")
     buttonLayout[i+1,5] = Label(scene, "$(newPortfolio[stock.ticker])")
 end
 #buttonLayout[2:(numberOfVisibleOptions+1),2:(1 + size(scrollButtonLayout)[2])] = scrollButtonLayout
-buttonLayout[div(numberOfVisibleOptions,2)+1,1] = upButton = Button(scene, label = "↑")
-buttonLayout[div(numberOfVisibleOptions,2)+2,1] = downButton = Button(scene, label = "↓")
 buttonLayout[1,2:5] = [Label(scene, "Stock"), Label(scene, "Owned"), Label(scene, "To Buy"), Label(scene, "Result")]
 #rowsize!(buttonLayout,2, Relative(numberOfVisibleOptions/(numberOfVisibleOptions+1)/2))
 #rowsize!(buttonLayout,3, Relative(numberOfVisibleOptions/(numberOfVisibleOptions+1)/2))
 
 
-firstVisible = Node(1)
 scrollButtonVisible = Node([1,numberOfVisibleOptions])
 scrollButtonLock = Node(true)
 function updateScrollList()
@@ -38,36 +43,14 @@ function updateScrollList()
         button.label = "$(IBIndex[j][1].ticker)"
         ownedLabel = content(buttonLayout[i+1,3])
         ownedLabel.text = "$(currentPortfolio[IBIndex[j][1].ticker])"
-        toBuyLabel = content(buttonLayout[i+1,3])
+        toBuyLabel = content(buttonLayout[i+1,4])
         toBuyLabel.text = "$(toBuy[IBIndex[j][1].ticker])"
-        resultLabel = content(buttonLayout[i+1,3])
+        resultLabel = content(buttonLayout[i+1,5])
         resultLabel.text = "$(newPortfolio[IBIndex[j][1].ticker])"
     end
 end
 
-on(downButton.clicks) do x
-    if (scrollButtonLock[])
-        scrollButtonLock[] = false
-        if scrollButtonVisible[][2] < length(IBIndex)
-            scrollButtonVisible[] .+= 1
-            updateScrollList()
-        end
-        println(x, ", ", scrollButtonVisible[])
-        scrollButtonLock[] = true
-    end
-end
-on(upButton.clicks) do x
-    if (scrollButtonLock[])
-        scrollButtonLock[] = false
-        if scrollButtonVisible[][1] > 1
-            scrollButtonVisible[] .-= 1
-            updateScrollList()
-        end
-        println(x, ", ", scrollButtonVisible[])
-        scrollButtonLock[] = true
-    end
-end
-buttonLayout[:,1] = slider = Slider(scene, range = (length(IBIndex)):(-1):(numberOfVisibleOptions), horizontal = false, startvalue = (1))
+buttonLayout[2:end,1] = slider = Slider(scene, range = (length(IBIndex)):(-1):(numberOfVisibleOptions), horizontal = false, startvalue = (1))
 prevSliderVal = slider.value[]
 sliderLock = Node(true)
 on(slider.value) do x
@@ -87,6 +70,27 @@ end
 #upButton.clicks[] = 0
 #downButton.clicks[] = 0
 
+changeCurrentPortfolioLayout = GridLayout(tellheight = false, valign = :top)
+changeCurrentPortfolioLayout[1,1] = Label(scene, highlightedStockTicker)
+highlightedOwnedNumber = lift((x)-> "Owned: $(currentPortfolio[x])", highlightedStockTicker)
+changeCurrentPortfolioLayout[2,1] = Label(scene, highlightedOwnedNumber)
+changeCurrentPortfolioLayout[1:2,2] = increaseOwnedButtons = [Button(scene,label =  "+"^i) for i = 1:2]
+changeCurrentPortfolioLayout[1:2,3] = decreaseOwnedButtons = [Button(scene,label =  "-"^i) for i = 1:2]
+for (i, button) in enumerate(increaseOwnedButtons)
+    on(button.clicks) do x
+        currentPortfolio[highlightedStockTicker[]] += 10^(i-1)
+        highlightedOwnedNumber[] = "Owned: $(currentPortfolio[highlightedStockTicker[]])"
+        updateScrollList()
+    end
+end
+for (i, button) in enumerate(decreaseOwnedButtons)
+    on(button.clicks) do x
+        currentPortfolio[highlightedStockTicker[]] =  maximum([0, currentPortfolio[highlightedStockTicker[]] - 10^(i-1)])
+        highlightedOwnedNumber[] = "Owned: $(currentPortfolio[highlightedStockTicker[]])"
+        updateScrollList()
+    end
+end
+layout[2,2] = changeCurrentPortfolioLayout
 
 indexBarAx = Axis(scene, xticks = (1:length(IBIndex), [s.ticker for (s,w) in IBIndex]), xticklabelrotation = pi/2*0.7, title = "Current IB-Index")
 #xtickrotation!(90)
@@ -95,8 +99,10 @@ layout[1,1] = indexBarAx
 
 portfolioBarAx = Axis(scene, xticks = (1:length(IBIndex), [s.ticker for (s,w) in IBIndex]), xticklabelrotation = pi/2*0.7, title = "Current Portfolio distribution")
 
-totalPortfolioValue = sum([s.price * currentPortfolio[s.ticker] for (s,w) in IBIndex])
-barplot!(portfolioBarAx, 1:length(currentPortfolio), [100*s.price*currentPortfolio[s.ticker]/totalPortfolioValue for (s,w) in IBIndex], color = 1:length(IBIndex))
+totalPortfolioValue = Node(sum([s.price * currentPortfolio[s.ticker] for (s,w) in IBIndex]))
+portfolioDistribution = lift(x->[100*s.price*currentPortfolio[s.ticker]/x for (s,w) in IBIndex], totalPortfolioValue)
+portfolioBarAx
+barplot!(portfolioBarAx,portfolioDistribution, color = 1:length(IBIndex))
 layout[2,1] = portfolioBarAx
 
 rowsize!(layout, 1, Relative(0.5))
